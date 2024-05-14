77af2416570b9b8c7d431f57067bdc7000f22eaf
Dynamicssquareca <dynamicssquareca@gmail.com>
2024-05-14 21:23:02 +0530

    update contant

--- pages/solutions.js
+++ pages/solutions.js
@@ -1,6 +1,6 @@
 import ModelBox from '@/components/ModelBox';
 import React from 'react';
-
+import Image from 'next/image';
 const Solution = () => {
     return (
         <>
@@ -16,17 +16,47 @@ const Solution = () => {
                         </div>
                     </div>
                     <div className='row'>
-                        <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                            <path fill="#DDEDF3" d="M51,-48.9C67.1,-34.8,82,-17.4,84.5,2.5C87,22.3,77,44.7,60.8,60.6C44.7,76.5,22.3,86,3.2,82.8C-15.9,79.6,-31.9,63.7,-47.5,47.8C-63,31.9,-78.3,15.9,-78,0.3C-77.8,-15.4,-62,-30.8,-46.4,-44.9C-30.8,-58.9,-15.4,-71.6,1,-72.6C17.4,-73.6,34.8,-62.9,51,-48.9Z" transform="translate(100 100)" />
-                        </svg>
                         <div class="elementor-background-overlay">
-                            <img src="/img/banner/solutions-banner-1.png" className='bg-top' alt="solutions-banner-1" />
-                            <img src="/img/banner/solution-banner-bg-1.png" className='bg-back' alt="solution-banner-bg-1" />
+                            <Image src="/img/icons/cloud-hero-bg.png" alt='cloud-hero-bg' className='bg-top' width={434} height={313} />
+                            <Image src="/img/banner/solutions-banner-1.png" className='bg-mid' alt="solutions-banner-1" width={993} height={278} />
+                            <Image src="/img/banner/solution-banner-bg-1.png" className='bg-back' alt="solution-banner-bg-1" width={1696} height={298} />
                         </div>
                     </div>
                 </div>
 
             </div>
+
+            <section className='bg-white'>
+                <div className='container'>
+                    <div className="row">
+                        <div className="col-lg-6">
+                            <div className="heading-style-two">
+                                <span>Key Features of Salesforce Sales Cloud</span>
+                                <h2>Equips your team with the tools they need to drive sales success</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col-lg-8'>
+                            <div className='row'>
+                                <div className='col-lg-4'>
+                                    <div className='service-box-three'>
+                                       <div className='contant-innr'>
+                                        <h3>Lead Management</h3>
+                                        <p>Capture, nurture, and convert leads seamlessly.</p>
+                                       </div>
+                                       <div className='contant-innr'>
+                                        <h3>Lead Management</h3>
+                                        <p>Capture, nurture, and convert leads seamlessly.</p>
+                                       </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
         </>
     );
 }
