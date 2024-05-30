5c685738deb07d689fd21bc320732cb1d810eca3
Dynamicssquareca <dynamicssquareca@gmail.com>
2024-05-30 15:22:37 +0530

    update page

--- components/ContactUSForm.js
+++ components/ContactUSForm.js
@@ -18,13 +18,13 @@ const ContactUSForm = () => {
             <div className='row'>
                 <div className='col-lg-6 align-self-center'>
                     <div className='form-left'>
-                        <div className='info-one'>
+                        {/* <div className='info-one'>
                             <span>Customer Support</span>
                             <p>Our dedicated support team is available to assist<br/> you with any inquiries or issues you may have. <br/><b>Contact us via email at</b>: <br/><a href='mailto:support@yourcompany.com'>support@yourcompany.com</a></p>
-                        </div>
+                        </div> */}
                         <div className='info-one'>
                             <span>For Sales Enquiry</span>
-                            <p><a href='mailto:support@yourcompany.com'>sales@yourcompany.com.</a></p>
+                            <p><a href='mailto:info@crmforceplus.com'>info@crmforceplus.com</a></p>
                         </div>
                         <div className='info-one'>
                             <span>Contact Number</span>
@@ -32,10 +32,7 @@ const ContactUSForm = () => {
                         </div>
                         <div className='info-one'>
                             <span>Visit Us</span>
-                            <p>123 Main Street,<br/>
-                                City, State,<br/>
-                                Zip Code
-                            </p>
+                            <p>2372 Morse Ave, <br/>Ste. 310 Irvine, <br/>CA 92614</p>
                         </div>
                     </div>
                 </div>
